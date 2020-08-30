58467b223e42ef9c540f7e3866ae502bdc04df68
syedasmatullah <58637197+syedasmatullah@users.noreply.github.com>
2020-08-30 20:35:28 +0500

    Updated

--- app/src/main/java/com/example/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/higherorlower/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     int randomNumber;
 
     private EditText maxNumber, enteredNumber;
-    private TextView myTextview;
+    private TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,18 +29,18 @@ public class MainActivity extends AppCompatActivity {
         Button guessButton = findViewById(R.id.guessButtonId);
         maxNumber = findViewById(R.id.maxNumberId);
         enteredNumber = findViewById(R.id.enteredNumberId);
-        myTextview = findViewById(R.id.tellRangetextviewId);
+        myTextView = findViewById(R.id.tellRangetextviewId);
         Button setRangeBtn = findViewById(R.id.setrangebtnid);
 
         setRangeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (maxNumber.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this, "Enter Max Number First!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, R.string.enter_max_number, Toast.LENGTH_SHORT).show();
                 }else {
 
-                    String mytext = "I'm thinking of a number between 1 and " + maxNumber.getText().toString();
-                    myTextview.setText(mytext);
+                    String mytext = getString(R.string.number_between) + maxNumber.getText().toString();
+                    myTextView.setText(mytext);
                     generateRandomNum();
                 }
 
@@ -53,27 +53,27 @@ public class MainActivity extends AppCompatActivity {
 
                 if (maxNumber.getText().toString().isEmpty()){
 
-                    Toast.makeText(MainActivity.this, "Enter Max Number First!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, R.string.enter_max_first, Toast.LENGTH_SHORT).show();
 
                 }else if (enteredNumber.getText().toString().isEmpty()){
 
-                    Toast.makeText(MainActivity.this, "Enter Guess Number!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, R.string.enter_guessed_number, Toast.LENGTH_SHORT).show();
 
                 }else{
                     int guessValue = Integer.parseInt(enteredNumber.getText().toString());
 
                     if (guessValue > randomNumber){
 
-                        Toast.makeText(MainActivity.this, "Lower than " + guessValue, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, getString(R.string.lower_than) + guessValue, Toast.LENGTH_SHORT).show();
                         enteredNumber.setText("");
 
                     }else if (guessValue < randomNumber){
 
-                        Toast.makeText(MainActivity.this, "Higher than " + guessValue, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, getString(R.string.higher_than) + guessValue, Toast.LENGTH_SHORT).show();
                         enteredNumber.setText("");
                     }else {
 
-                        Toast.makeText(MainActivity.this, "Congrats! You guessed it right", Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, R.string.congrats, Toast.LENGTH_LONG).show();
                         enteredNumber.setText("");
                         generateRandomNum();
                     }
@@ -86,9 +86,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void generateRandomNum(){
 
-        int maxnum = Integer.parseInt(maxNumber.getText().toString());
+        int maxNum = Integer.parseInt(maxNumber.getText().toString());
 
         Random Rand = new Random();
-        randomNumber = Rand.nextInt(maxnum) + 1;
+        randomNumber = Rand.nextInt(maxNum) + 1;
     }
 }
